4d6aedeac58d468acc4a2c47d100ac3d19fce8d4
Özge Eser <ozge.eser@bilgiedu.net>
2018-10-07 14:11:40 +0300

    Add files via upload

---
+++ 2numsum.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)			
+        {
+			int num1;
+			int num2;
+			int res;
+				Console.WriteLine("Please write first number");
+				num1 = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Please write second number");
+				num2 = Convert.ToInt32(Console.ReadLine());
+				res = num1 + num2;
+				Console.WriteLine("Result is:" + result);
+
+				
+		}
+	}
+}
---
+++ year.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)			
+        {
+			Console.WriteLine("Introduction to Programming\n\nDigital Game Design\n\nWhat year we're at?\n");
+				String year = Console.ReadLine();
+				Console.WriteLine("You're at " + year + " now.");
+				String word;
+				word = Console.ReadLine();
+				Console.WriteLine("The message is: " + word);
+				
+		}
+	}
+}
